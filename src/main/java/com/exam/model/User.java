50e971f2d2264fcd5d866b396cd4ebdc46a414ea
747808 <tanviruge2000@gmail.com>
2023-09-21 21:44:42 +0530

    first commit

---
+++ src/main/java/com/exam/model/User.java
@@ -0,0 +1,73 @@
+package com.exam.model;
+
+public class User{
+    private Long id;
+    private String username;
+    private String firstname;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    private String lastname;
+    private String email;
+    private String phone;
+ private boolean enabled =true;
+
+//this is user
+
+
+
+}
